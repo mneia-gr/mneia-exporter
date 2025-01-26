195568a7f5ead9dcb7084fb46ca55c4be89cfac9
Marios Zindilis <marios@zindilis.com>
2025-01-26 10:09:58 +0000

    Added functionality to export as YAML.

--- mneia_exporter/management/commands/export.py
+++ mneia_exporter/management/commands/export.py
@@ -52,6 +52,35 @@ def export_json():
             instance.export_json()
 
 
+def export_yaml():
+    """
+    Exports all instances of all models of Mneia Backend to YAML files.
+
+    Lots of terminal printing stuff here, not very pretty. Unicode characters taken from:
+    https://j2r2b.github.io/2020/07/23/text-representation-of-trees.html
+    """
+
+    models = list(apps.get_app_config(MneiaBackendConfig.name).get_models())
+
+    for model_index, model in enumerate(models):
+        if model_index + 1 == len(models):  # last model in the list
+            cprint("\u2514", "blue", attrs=["bold"], end="")
+        else:
+            cprint("\u251c", "blue", attrs=["bold"], end="")
+        cprint("\u2500\u2500 Model: ", "blue", attrs=["bold"], end="")
+        print(f"{model.__name__} ({model_index+1}/{len(models)})")
+
+        instances = list(model.objects.all())
+        for instance_index, instance in enumerate(instances):
+            if model_index + 1 == len(models):  # last model in the list
+                print(" ", end="")
+            else:
+                cprint("\u2502", "blue", attrs=["bold"], end="")
+            cprint("   \u2514\u2500\u2500 Instance: ", "blue", attrs=["bold"], end="")
+            print(f'{instance.id}: "{instance}" ({instance_index+1}/{len(instances)})')
+            instance.export_yaml()
+
+
 class Command(BaseCommand):
     help = "Exports Mneia data"
 
@@ -61,3 +90,6 @@ class Command(BaseCommand):
 
         cprint("MNEIA EXPORTER -> DOCS", "blue", attrs=["bold"])
         export_docs()
+
+        cprint("MNEIA EXPORTER -> YAML", "blue", attrs=["bold"])
+        export_yaml()
